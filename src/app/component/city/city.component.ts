5311a8bc818999a101fbe89a4e8cbd7668765910
fuadsadiqov <sadiqovfuad8@gmail.com>
2023-07-21 20:48:19 +0400

    adding new functionality

--- src/app/component/city/city.component.ts
+++ src/app/component/city/city.component.ts
@@ -2,9 +2,10 @@ import { Component, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SimplesCountryMap } from '../../map'
 import { RestService } from 'src/app/services/rest.service';
-
+import { ChangeDegree } from 'src/convertKelvinToCelcies';
+ 
 // Ng Chart
-import { Chart, ChartConfiguration, ChartType } from 'chart.js';
+import { Chart, ChartConfiguration, ChartEvent, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 
@@ -17,9 +18,10 @@ export class CityComponent {
   public coutriesList = Object.entries(SimplesCountryMap.state_specific).map(item => item)
   public country: any
   public countryWeather: any
-  public currentIndex: number = 1
-  public dailyWeatherWrapper: Array<any> = []
-
+  public allDayWeather: Array<any> = []
+  public fiveDayHourlyWeaher: Array<any> = []
+  public math = Math
+  
   constructor(private route: ActivatedRoute, private restService: RestService, private router: Router){
     this.getWeather()
   }
@@ -29,27 +31,32 @@ export class CityComponent {
       this.country = this.coutriesList.find(item => item[0] == id)      
       this.restService.getWeather(this.country[1].name)
       .subscribe((res: any) => {
-        console.log(res);        
-        res.list.filter((item: any) => {
-          if(item.dt_txt.substr(-8) == '00:00:00'){
-            this.dailyWeatherWrapper = [...this.dailyWeatherWrapper, item] 
-          }
-        })
-        this.lineChartData.labels = res.list.map((item: any) => item.dt).slice(res.list, this.currentIndex * 8)
-        this.lineChartData.datasets[0].data = res.list.map((item: any) => item.main.temp).slice(res.list, this.currentIndex * 8)
+        this.fiveDayHourlyWeaher = res.list
+        let lastEightDayData =  res.list.map((item: any) => Math.round(item.main.temp - 273.15)).slice(0, 8)
+        let lastEightDayLabel =  res.list.map((item: any) => item.dt_txt).slice(0, 8)
+        this.lineChartData.labels = lastEightDayLabel 
+        this.lineChartData.datasets[0].data = lastEightDayData
         this.countryWeather = res.list[0]
-
-        this.countryWeather.main.temp = Math.round(this.countryWeather.main.temp - 273.15)
-      })
+        this.allDayWeather = res.list.filter((item: any) => item.dt_txt.substr(-8) == '12:00:00')
+        })
       })
   }
   navigateToCity(cityId: string){
     this.router.navigateByUrl('city/' + cityId)
     this.chart?.update()
-    this.dailyWeatherWrapper = []
     this.getWeather()
   }
-
+  changeTime(time: string){
+    this.countryWeather = this.allDayWeather.find((item: any) => item.dt_txt == time)
+    let lineChartArray = this.fiveDayHourlyWeaher.filter((item: any) => item.dt_txt.substr(8, 2) == time.substring(8, 10))    
+    this.lineChartData.labels = lineChartArray.map((item: any) => item.dt_txt)
+    this.lineChartData.datasets[0].data = lineChartArray.map((item: any) => Math.round(item.main.temp - 273.15))
+    console.log(this.fiveDayHourlyWeaher.map((item: any) => item.main.temp));
+    this.chart?.update()
+  }
+  public chartClicked({ event, active}: { event?: ChartEvent, active?: object[]}): void {
+    console.log(event, active);    
+  }
   // Ng Chart
   public lineChartData: ChartConfiguration['data'] = {
     datasets: [
@@ -75,7 +82,17 @@ export class CityComponent {
     responsive: true,
     aspectRatio: 5,
     scales: {
+      x:{
+        ticks: {
+          color: "white",
+          callback: function(val: any, index) {
+            let changedValue: any = this.getLabelForValue(val).substring(10, 16)
+            return this.getLabelForValue(changedValue);
+          },
+        }
+      },
       y: {
+          beginAtZero: true,
           display: false,
         },
     },
---
+++ src/convertKelvinToCelcies.ts
@@ -0,0 +1,5 @@
+export function ChangeDegree(kelvin: number): number{
+    let celcies = Math.round(kelvin - 273.15)
+
+    return celcies
+}
